f5be290eafb027b0677a010ba0eea1b1e9df81c9
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-11 17:29:36 +0530

    modified:   Arena/Max_freq.java

--- Arena/Max_freq.java
+++ Arena/Max_freq.java
@@ -1,3 +1,31 @@
+ /* Max freq
+asked in interviews by 1 company
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given an array of N integers, give the number with maximum frequency. If multiple numbers have maximum frequency print the maximum number among them.
+Input
+The first line of the input contains an integer N, Second lines contains N space seperated integers of array.
+
+Constraints
+3 <= N <= 1000
+1 <= Arr[i] <= 100
+Output
+The output should contain single integer, the number with maximum frequency.If multiple numbers have maximum frequency print the maximum number among them.
+Example
+Sample Input
+5
+1 4 2 4 5
+
+Sample Output
+4
+
+Explanation:-4 has max frequency=2
+
+*/
+
+
+
 import java.util.HashMap;
 import java.util.Scanner;
 
