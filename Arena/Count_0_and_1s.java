bf5c51a1176b21f481ea9f02d4f9e617429f9e96
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-11 18:42:15 +0530

    new file:   Arena/Count_0_and_1s.java

---
+++ Arena/Count_0_and_1s.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+/*
+Count of 0's and 1's
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given a binary array of size N. Count number of 1's and 0's in the array.
+Input
+First line contains n.
+Next line contains n space separated integers.
+
+Constraints
+1 <= N <= 105
+0 <= arr[i] <= 1
+Output
+Print two integers, count of 1s and count of 0s.
+Example
+Input:
+5
+0 1 1 0 1
+
+Output:
+3 2
+ */
+
+
+public class Count_0_and_1s {
+     public static void main(String[] args) {
+          try (Scanner sc = new Scanner(System.in)) {
+
+               int n = sc.nextInt();
+               int arr[] = new int[n];
+
+               for(int i =0 ; i<n; i++){
+                    arr[i]=sc.nextInt();
+               }
+               int a=0,b=0;
+               for(int elem1:arr){
+                    if(elem1==1){
+                         a++;
+                    }
+               }
+
+               for(int elem0:arr){
+                    if(elem0==0){
+                         b++;
+                    }
+               }
+
+               System.out.print(a+" ");
+               System.out.print(b+" ");
+          }
+          
+     }
+}
