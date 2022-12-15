bd4178510f2bc8b0a4aee8276fe74bd2c619709e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-15 16:19:33 +0530

    modified:   afterISA/Help_Megha_Prime_Num.java

--- afterISA/Help_Megha_Prime_Num.java
+++ afterISA/Help_Megha_Prime_Num.java
@@ -30,8 +30,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+// import java.util.Scanner;
+// import java.util.StringTokenizer;
 
 class Help_Megha_Prime_Num {
 
