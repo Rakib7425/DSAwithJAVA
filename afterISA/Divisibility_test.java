01c636bed783d0ba1391c38674e7daa4197f22c6
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-16 21:25:21 +0530

    modified:   afterISA/.vscode/launch.json 	new file:   afterISA/Divisibility_test.java

---
+++ afterISA/Divisibility_test.java
@@ -0,0 +1,49 @@
+
+// import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Divisibility_test {
+     public static void main (String[] args) {
+          // Your code here
+          try (Scanner sc = new Scanner(System.in)) {
+              String number = sc.nextLine();
+                      if((number.charAt(number.length()-1)=='0')){
+                            System.out.println("Yes");
+                      }
+                      else {
+                       System.out.println("No");
+  
+                      }
+                        
+          }
+      
+     }
+}
+
+/*
+ * 
+ * /*
+ * Divisibility Test
+ * Easy
+ * 
+ * Time Limit: 2 sec
+ * Memory Limit: 128000 kB
+ * Problem Statement
+ * You are given a big number in form of a string A of characters from 0 to 9.
+ * Return 1 if it's divisible by 30 and 0 otherwise.
+ * Input
+ * The first argument is the string A.
+ * Output
+ * Return 1 if it is divisible by 30 and 0 otherwise.
+ * Note It's a functional Problem you just have to return 0 or 1.
+ * Example
+ * Sample Input :
+ * 3033330
+ * 
+ * Sample Output:
+ * Yes
+ */
+
+*/
