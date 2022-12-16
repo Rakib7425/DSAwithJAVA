c3cc66062dca79f01dbc6756698dd28a8ea596f4
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-16 21:30:38 +0530

    modified:   afterISA/Divisibility_test.java

--- afterISA/Divisibility_test.java
+++ afterISA/Divisibility_test.java
@@ -1,25 +1,24 @@
-
 // import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
 // don't change the name of this class
 // you can add inner classes if needed
 class Divisibility_test {
-     public static void main (String[] args) {
-          // Your code here
-          try (Scanner sc = new Scanner(System.in)) {
-              String number = sc.nextLine();
-                      if((number.charAt(number.length()-1)=='0')){
-                            System.out.println("Yes");
-                      }
-                      else {
-                       System.out.println("No");
-  
-                      }
-                        
-          }
-      
-     }
+    public static void main (String[] args) {
+        // Your code here
+        try (Scanner sc = new Scanner(System.in)) {
+            String number = sc.nextLine();
+                    if((number.charAt(number.length()-1)=='0')){
+                          System.out.println("Yes");
+                    }else {
+            
+                     System.out.println("No");
+
+                    }
+                      
+        }
+    
+    }
 }
 
 /*
