c70cab05245a2a67f0dcd5cdc6efc473e3107a3e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-16 14:41:59 +0530

    new file:   afterISA/Print_Patern.java 	new file:   afterISA/Tower_of_Hanoi.java 	new file:   afterISA/Tribonacci_Number_Easy_version_Recursion.java

---
+++ afterISA/Tribonacci_Number_Easy_version_Recursion.java
@@ -0,0 +1,69 @@
+
+// import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class Tribonacci_Number_Easy_version_Recursion {
+     // don't change the name of this class
+     // you can add inner classes if needed
+
+     public static int fibo(int n) {
+
+          if (n == 1 || n == 0) {
+               return 0;
+          }
+          if (n == 2 || n == 3) {
+               return n - 2;
+          }
+
+          n = fibo(n - 1) + fibo(n - 2) + fibo(n - 3);
+
+          return n;
+          // return fibo(n) = fibo(n-1) + fibo(n-2) + fibo(n-3);
+
+     }
+
+     public static void main(String[] args) {
+          try (Scanner sc = new Scanner(System.in)) {
+               int num = sc.nextInt();
+
+               System.out.print(fibo(num));
+               // fibo(num);
+          }
+     }
+
+}
+
+/*
+ * 
+ * 
+ * Tribonacci Number: Easy-version (Recursion)
+ * medium
+ * Time Limit: 2 sec
+ * Memory Limit: 128000 kB
+ * Problem Statement
+ * Fibonacci numbers are interesting but Tribonacci numbers are more
+ * interesting.
+ * A Tribonacci number T(n) is the sum of the preceding three elements in a
+ * series. Consider its first three numbers to be 0, 0, and 1. i. e T(1) = 0,
+ * T(2) = 0, T(3) = 1.
+ * Given a number N, your task is to return the nth Tribonacci number.
+ * Input
+ * The input contains a single integer N.
+ * 
+ * Constraints:-
+ * 1 <= N <= 20
+ * Output
+ * Return the Nth Tribonacci number.
+ * Example
+ * Sample Input:-
+ * 4
+ * 
+ * Sample Output:-
+ * 1
+ * 
+ * Sample Input:-
+ * 7
+ * 
+ * Sample Output:-
+ * 7
+ */
