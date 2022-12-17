282d744d086cf4677c004728bfab6b55797bac6e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-18 00:39:15 +0530

    modified:   afterISA/.vscode/launch.json 	new file:   afterISA/Quiz/Test.java 	new file:   afterISA/Quiz/Testing.html

---
+++ afterISA/Quiz/Test.java
@@ -0,0 +1,34 @@
+package Quiz;
+
+public class Test {
+     public static void main(String[] args) {
+
+          /*
+          //  !q1
+           
+          // ? What does the following piece of code do?
+
+          int arr[] = { 56, 45, 84, 15, 11, 55, 45, 15, 4, 48, 4, 5, 22, 5, 33, 44, 8, 55, 15, 5, 45, 66, 77, 4, 5, 51,88, 4, 7, 4, 99 };
+
+          for (int i = 0; i < arr.length - 1; i++) {
+               for (int j = i + 1; j < arr.length; j++) {
+                    if ((arr[i].equals(arr[j])) && (i != j)) {
+                         System.out.println(arr[i]);
+                    }
+               }
+          }
+          // ! q1 ends
+          */
+
+          /*
+           ! q2
+
+           ? Given an array arr = {5,6,77,88,99} and key = 88; How many iterations are done until the element is found?
+
+           */
+
+
+           char[] str={'i','n'};
+           System.out.println(str.toString());
+     }
+}
