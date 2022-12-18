0c609e2a7b569a46de642f4481fbeb137ec397fb
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-18 20:32:31 +0530

    modified:   afterISA/.vscode/launch.json 	new file:   afterISA/Repeated_characters_and_their_count.java

---
+++ afterISA/Repeated_characters_and_their_count.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+/**
+ * Repeated_characters_and_their_count
+ */
+public class Repeated_characters_and_their_count {
+
+     public static void main(String[] args) {
+
+          try (Scanner sc = new Scanner(System.in)) {
+               
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               // int n = sc.nextInt();
+
+               // char ch[] = sc.next().toCharArray();
+
+               // String s = sc.nextLine();
+
+               // for (int i = 0; i < n; i++) {
+               //      ch[i] = s.charAt(i); // Input in CharArray
+               // }
+
+               // for (int i = 0; i < n ; i++) {
+               //      System.out.print(ch[i]); // Output of CharArray
+               // }
+          }
+     }
+}
