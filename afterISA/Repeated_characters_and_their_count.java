1dc013cc3d7c5adfd2a659178ef554875a1b6b41
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-18 23:24:44 +0530

    modified:   afterISA/Repeated_characters_and_their_count.java

--- afterISA/Repeated_characters_and_their_count.java
+++ afterISA/Repeated_characters_and_their_count.java
@@ -8,13 +8,13 @@ import java.util.*;
 public class Repeated_characters_and_their_count {
      static final int MAX_CHAR = 256;
 
-     static void getOccuringChars(int length, String st) {
+     static void getOccuringChars(String st, int length) {
           // an array was created with the size of 256 ( Ascii values)
           int counts[] = new int[MAX_CHAR];
           // a variable for finding the length of the String given as input
-           length = st.length();
+          length = st.length();
           // int length = st.length();
-          
+
           // the index value of the count array is initialized
           for (int i = 0; i < length; i++)
                counts[st.charAt(i)]++;
@@ -31,7 +31,7 @@ public class Repeated_characters_and_their_count {
                if (finds == 1)
                     // display the character count
                     if (counts[st.charAt(i)] > 1) {
-                         
+
                          System.out.println(st.charAt(i) + " " + counts[st.charAt(i)]);
                     }
           }
@@ -40,18 +40,18 @@ public class Repeated_characters_and_their_count {
      // main section of the program
      public static void main(String args[]) {
           try (Scanner sc = new Scanner(System.in)) {
-
                int n = sc.nextInt();
+               String str = sc.next();
+
                // String st = "banana"; // a string
-                    // String st = new String();
-               
-               String str = sc.nextLine();
-               
+               // String st = new String();
+
                // function calling
-               getOccuringChars(n,str);
-               
+               getOccuringChars(str, n);
           }
+
      }
+
 }
 
 /*
