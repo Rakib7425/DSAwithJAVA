8d8e48ea41d9d771318e0f0fd1318a0290f8b42e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-20 21:17:42 +0530

    modified:   afterISA/AB_String.java

--- afterISA/AB_String.java
+++ afterISA/AB_String.java
@@ -61,3 +61,49 @@ class AB_String {
           }
      }
 }
+
+/*
+ * 
+ ? AB strings
+ ? medium
+ ? Time Limit: 2 sec
+ ? Memory Limit: 128000 kB
+ ? Problem Statement
+ ? A string is called AB string if it's of length at least 2 and all it's
+ ? characters are A except the last character which is B.
+ ? 
+ ? You have an empty string s, you can do the following operation any number of
+ ? times.
+ ? 
+ ? Choose any position of s and insert any AB string in that position.
+ ? 
+ ? You are given a string t of some length containing only A and B. Find out if
+ ? it is possible to convert s to t after some operations.
+ ? Input
+ ? First line contains N denoting the length of t.
+ ? Next line contains t.
+ ? 
+ ? Constraints
+ ? 1 <= N <= 105
+ ? t contains only A and B.
+ ? Output
+ ? Output "YES" or "NO".
+ ? Example
+ ? Input:
+ ? 5
+ ? AAABB
+ ? 
+ ? Output:
+ ? YES
+ ? 
+ ? Explanation :
+ ? "" => "AAB" => "AA "AB" B" => "AAABB"
+ ? 
+ ? 
+ ? Input:
+ ? 3
+ ? ABB
+ ? 
+ ? Output:
+ ? NO
+ */
