97533df1ee1733af93521e71ed30efb734fa5ee2
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-29 18:47:38 +0530

    new file:   afterISA/Count_Smaller_Numbers.java 	new file:   afterISA/Sort_by_Absolute_Difference.java 	new file:   afterISA/Wave_Array.java

---
+++ afterISA/Count_Smaller_Numbers.java
@@ -0,0 +1,20 @@
+
+
+//import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+
+public class Count_Smaller_Numbers {
+     public static void main(String[] args) {
+     //Main Method is here
+          try (Scanner sc = new Scanner(System.in)) {
+               int n = sc.nextInt();
+               int arr[]= new int[n];
+
+               for (int i = 0; i < n; i++) {
+                    arr[i] = sc.nextInt();
+               }
+
+          }
+     }
+}
