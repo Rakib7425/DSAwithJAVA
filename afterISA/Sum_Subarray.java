dfec45f5173f0e085bdc821b23e542e3ce25634a
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-01-05 13:29:21 +0530

    modified:   afterISA/Sum_Subarray.java

--- afterISA/Sum_Subarray.java
+++ afterISA/Sum_Subarray.java
@@ -3,6 +3,14 @@
 import java.util.*; // contains Collections framework
 
 public class Sum_Subarray {
+     // helper function to calculate sum of elements in array
+     public static int sum(int[] arr, int start, int end) {
+          int sum = 0;
+          for (int i = start; i <= end; i++) {
+               sum += arr[i];
+          }
+          return sum;
+     }
 
      public static void main(String[] args) {
 
@@ -14,19 +22,21 @@ public class Sum_Subarray {
                for (int i = 0; i < n; i++) {
                     arr[i] = sc.nextInt();
                }
-
                
-               int sum = 0;
-               for (int i = 0; i < n; i++) {
-                    for (int j = n - 1; j >= i; j--) {
-                         if (arr[i] + arr[j] >= k) {
-                              sum = arr[i] + arr[j];
-                              break;
-                         }
-
+               int min_length = Integer.MAX_VALUE; // initialize minimum length to maximum integer value
+               int start = 0; // initialize start of subarray
+               int end = 0; // initialize end of subarray
+               while (end < arr.length) { // iterate through array
+                    if (sum(arr, start, end) >= k) { // if sum is greater than or equal to k
+                         min_length = Math.min(min_length, end - start + 1); // update min length if necessary
+                         start += 1; // increment start of subarray
+                    } else {
+                         end += 1; // increment end of subarray
                     }
                }
-               System.out.print(sum);
+
+               System.out.println(min_length);
+
           }
      }
 }
