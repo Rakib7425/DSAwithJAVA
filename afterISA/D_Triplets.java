9eca68106826c0a98b13b1a30668070b9fbaeb81
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-01-09 23:21:21 +0530

    modified:   afterISA/D_Triplets.java

--- afterISA/D_Triplets.java
+++ afterISA/D_Triplets.java
@@ -30,3 +30,43 @@ public class D_Triplets {
           }
      }
 }
+/*
+ * D Triplets
+ * easy
+ * Time Limit: 2 sec
+ * Memory Limit: 128000 kB
+ * Problem Statement
+ * Given an array Arr[] of size N as input, your task is to count the number of
+ * triplets Arr[i], Arr[j] and Arr[k] such that:-
+ * i < j < k and the difference between every 2 elements of triplets is less
+ * than or equal to P i. e |Arr[i] - Arr[j]| <= P, |Arr[i] - Arr[k]| <= P and
+ * |Arr[j] - Arr[k]| <= P
+ * Input
+ * The first line of input contains two space- separated integers N and P.
+ * next line contains N space separated integers depicting the values of the
+ * Arr[].
+ * 
+ * Constraints:-
+ * 3 <= N <= 105
+ * 1 <= Arr[i], P <= 109
+ * 0 <= i <= N-1
+ * Output
+ * Return the count of triplets that satisfies the above conditions.
+ * Example
+ * Sample Input:-
+ * 5 4
+ * 1 3 2 5 9
+ * 
+ * Sample Output:-
+ * 4
+ * 
+ * Explanation:-
+ * (1, 3, 2), (1, 3, 5), (1, 2, 5), (2, 3, 5) are the required triplets
+ * 
+ * Sample Input:-
+ * 5 3
+ * 1 8 4 2 9
+ * 
+ * Sample Output:-
+ * 1
+ */
