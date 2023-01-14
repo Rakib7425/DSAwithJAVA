05acb95b9e5f835d118956b80a5b74ffcc79f615
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-01-14 23:13:45 +0530

    new file:   afterISA/Creating_Object.java

---
+++ afterISA/Creating_Object.java
@@ -0,0 +1,33 @@
+
+    // Car1 carObj=new Car1();
+    // carObj.carName=carName;
+    // carObj.modelNum=modelNum;
+    // carObj.engineNum=engineNum;
+    
+    
+
+/*
+ * Creating Object
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given a class and some inputs for the corresponding variables of the class, your task is to create an object of the given class by the name Car1 and assign the given input to its variables.
+Input
+The input will contain 3 lines:-
+First- line contains a string which is the Name of the Car
+Second- line contains an integer which is the Model of the Car
+The last line of input contains an integer which is the Engine of the Car.
+Output
+You don't need to print anything printing will be done by the driver code itself, you just have to create an object of the class Car by the name Car1 and assign the variables given in the input.
+Example
+Sample Input:-
+Ferrari
+143
+123
+
+Sample Output:-
+Ferrari
+143
+123
+ */
