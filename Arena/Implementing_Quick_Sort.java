c1d0766d968d7d4cf88628042dfdec2af204618e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-01-24 22:53:45 +0530

    modified:   Arena/Implementing_Quick_Sort.java 	modified:   Arena/Square_Root_Of_an_Integer.java

--- Arena/Implementing_Quick_Sort.java
+++ Arena/Implementing_Quick_Sort.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Implementing_Quick_Sort {
 
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            sort(arr, 0, n - 1);
+            print(arr, n);
         }
-        sort(arr, 0, n - 1);
-        print(arr, n);
 
     }
 
