ef0bb34f2fd318d1d7a0ed93e19a816d37cc6dae
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-01-24 23:50:06 +0530

    modified:   Arena/Count_0_and_1s.java

--- Arena/Count_0_and_1s.java
+++ Arena/Count_0_and_1s.java
@@ -1,28 +1,4 @@
 import java.util.Scanner;
-/*
-Count of 0's and 1's
-Time Limit: 2 sec
-Memory Limit: 128000 kB
-Problem Statement
-Given a binary array of size N. Count number of 1's and 0's in the array.
-Input
-First line contains n.
-Next line contains n space separated integers.
-
-Constraints
-1 <= N <= 105
-0 <= arr[i] <= 1
-Output
-Print two integers, count of 1s and count of 0s.
-Example
-Input:
-5
-0 1 1 0 1
-
-Output:
-3 2
- */
-
 
 public class Count_0_and_1s {
      public static void main(String[] args) {
@@ -31,25 +7,48 @@ public class Count_0_and_1s {
                int n = sc.nextInt();
                int arr[] = new int[n];
 
-               for(int i =0 ; i<n; i++){
-                    arr[i]=sc.nextInt();
+               for (int i = 0; i < n; i++) {
+                    arr[i] = sc.nextInt();
                }
-               int a=0,b=0;
-               for(int elem1:arr){
-                    if(elem1==1){
+               int a = 0, b = 0;
+               for (int elem1 : arr) {
+                    if (elem1 == 1) {
                          a++;
                     }
                }
 
-               for(int elem0:arr){
-                    if(elem0==0){
+               for (int elem0 : arr) {
+                    if (elem0 == 0) {
                          b++;
                     }
                }
 
-               System.out.print(a+" ");
-               System.out.print(b+" ");
+               System.out.print(a + " ");
+               System.out.print(b + " ");
           }
-          
+
      }
 }
+/*
+ * Count of 0's and 1's
+ * Time Limit: 2 sec
+ * Memory Limit: 128000 kB
+ * Problem Statement
+ * Given a binary array of size N. Count number of 1's and 0's in the array.
+ * Input
+ * First line contains n.
+ * Next line contains n space separated integers.
+ * 
+ * Constraints
+ * 1 <= N <= 105
+ * 0 <= arr[i] <= 1
+ * Output
+ * Print two integers, count of 1s and count of 0s.
+ * Example
+ * Input:
+ * 5
+ * 0 1 1 0 1
+ * 
+ * Output:
+ * 3 2
+ */
