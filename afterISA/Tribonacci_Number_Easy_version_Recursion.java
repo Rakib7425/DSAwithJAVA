0ebe68b5fc6083aefd60df9694336f933ea5d598
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-02-01 11:59:39 +0530

    modified:   afterISA/Tribonacci_Number_Easy_version_Recursion.java

--- afterISA/Tribonacci_Number_Easy_version_Recursion.java
+++ afterISA/Tribonacci_Number_Easy_version_Recursion.java
@@ -36,6 +36,30 @@ public class Tribonacci_Number_Easy_version_Recursion {
 /*
  * 
  * 
+ * class Main {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int t0 = 0, t1 = 1, t2 = 1, tn = 0;
+        for (int i = 3; i <= n; i++) {
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+        return tn;
+    }
+}
+
+
+ */
+/*
+ * 
+ * 
  * Tribonacci Number: Easy-version (Recursion)
  * medium
  * Time Limit: 2 sec
