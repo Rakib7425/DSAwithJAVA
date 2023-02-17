71128c19122ac4d6d03c4cf5da6589de17afa801
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-02-17 23:31:46 +0530

    modified:   afterISA/Contests/22_01_2023/Addition_of_Common_Elements.java

--- afterISA/Contests/22_01_2023/Addition_of_Common_Elements.java
+++ afterISA/Contests/22_01_2023/Addition_of_Common_Elements.java
@@ -8,24 +8,25 @@ class Addition_of_Common_Elements {
     public static void main(String[] args) {
         // Your code here
         Set<Integer> set = new HashSet<>();
-        Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        int N1 = sc.nextInt();
-        int N2 = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int sum = 0;
+            int N1 = sc.nextInt();
+            int N2 = sc.nextInt();
 
-        for (int i = 0; i < N1; i++) {
-            int n = sc.nextInt();
-            set.add(n);
-        }
-
-        for (int i = 0; i < N2; i++) {
-            int n = sc.nextInt();
-            if (set.contains(n)) {
-                sum += n;
-                set.remove(n);
+            for (int i = 0; i < N1; i++) {
+                int n = sc.nextInt();
+                set.add(n);
             }
+
+            for (int i = 0; i < N2; i++) {
+                int n = sc.nextInt();
+                if (set.contains(n)) {
+                    sum += n;
+                    set.remove(n);
+                }
+            }
+            System.out.println(sum);
         }
-        System.out.println(sum);
     }
 }
 
