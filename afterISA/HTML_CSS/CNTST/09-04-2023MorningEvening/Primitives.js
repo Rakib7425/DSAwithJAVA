5f6166ff044c42283a5e8f43f3320511916ad122
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-11 00:41:04 +0530

    LoL

---
+++ afterISA/HTML_CSS/CNTST/09-04-2023MorningEvening/Primitives.js
@@ -0,0 +1,28 @@
+// Your code here
+let name = 'Raj';
+let age = 18;
+
+let greet = `My name is ${name} and I am ${age} years old`;
+// console.log(greet);
+
+/*
+Primitives
+easy
+Problem Statement
+Initialize two variables name and age with values of data types string and number respectively.
+Create another variable greet with the value of "My name is name and I am age years old" and print the value of greet in the console.
+
+
+Note: Generate Expected Output section will not work for this question
+DO NOT CONSOLE.LOG the variables, just declare them.
+Input
+There is no input required for this question
+Output
+There is no output required for this question
+ai_logoNEWTON AI
+BETA
+Simpler Explanation
+Concepts Used
+Example
+If value of name is Raj and value of age is 18, then greet should be storing "My name is Raj and I am 18 years old"
+*/
