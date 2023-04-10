58a90a959f21c2cd4b12dfc9d27730cd07528497
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-11 00:26:15 +0530

    First Cmd commmand

---
+++ afterISA/javaScript/JS_Primitive_Types.js
@@ -0,0 +1,42 @@
+// declare your variables below
+
+let varA = 7469;
+let varB = "Rakib";
+let varC = true;
+let varD = undefined;
+let varE = {
+    name: "Rakib",
+    gen: "Men",
+    roll_no: 69
+};
+let varF = Symbol("unique");
+
+/*
+JS Primitive Types
+easy
+Problem Statement
+Create 6 variables with namesvarA, varB, varC, varD, varE, varF
+Store 6 different values of your choice in the variables such that the variables represent the given primitive types in the given order:
+varA: Number
+varB: String
+varC: Boolean
+varD: Undefined
+varE: Object
+varF: Symbol
+Input
+No input is required, you just need to create the variables.
+Output
+.No output is required for this question.
+ai_logoNEWTON AI
+BETA
+Simpler Explanation
+Concepts Used
+Example
+.console.log(typeof varA); //prints number
+console.log(typeof varB); //prints stirng
+console.log(typeof varC); //prints boolean
+console.log(typeof varD); //prints undefined
+console.log(typeof varE); //prints object
+console.log(typeof varF); //prints symbol
+
+*/
