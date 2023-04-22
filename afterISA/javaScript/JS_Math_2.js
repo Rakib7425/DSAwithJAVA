a9b079424606a9e6b829f3c7075dccd18fe4a31e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-22 17:12:52 +0530

    new file:   afterISA/javaScript/JS_Math_2.js 	deleted:    afterISA/javaScript/tempCodeRunnerFile.javascript

---
+++ afterISA/javaScript/JS_Math_2.js
@@ -0,0 +1,29 @@
+// JS Math - 2
+function floor(inp) {
+    // write code here
+    // return the output , do not use console.log here
+    return Math.floor(inp);
+}
+/**
+ * JS Math - 2
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Implement the function floor, which should take a number which can be a float(decimal)
+and return its result as an integer with floor function applied to it (Use JS In built functions)
+Input
+Function will take a float as input
+Output
+Function will return a number
+ai_logoAI CODELENS
+BETA
+Simpler Explanation
+Concepts Used
+Example
+console.log(floor(1.99)) // prints 1
+
+console.log(floor(2.1)) // prints 2
+
+console.log(floor(-0.8)) // prints -1
+ */
--- afterISA/javaScript/tempCodeRunnerFile.javascript
+++
@@ -1,5 +0,0 @@
-var array = [2, 4, 6, 7, 8, 10];
-var myArr = array.filter(v => v / 2 === 0);
-
-console.log(myArr);
-//  console.log(calculator(2, 3, add));
