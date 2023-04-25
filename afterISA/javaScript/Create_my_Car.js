00820b78dd5b00531d64297e02dc611c271cbae7
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-25 16:01:37 +0530

    new file:   afterISA/javaScript/Create_my_Car.js

---
+++ afterISA/javaScript/Create_my_Car.js
@@ -0,0 +1,51 @@
+// Your code here
+const Car = {
+    color: "white",
+    seats: 2,
+    nitros: false
+}
+let msg = `My car have ${Car.seats} seats and it is of color ${Car.color}`;
+
+
+/**
+ * Create my Car
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+You need to create an Object in Javascript with the name Car.
+This object will have three properties with name color, seats, nitros
+The table given below contains the properties of the object, their data type, and the value for that property.
+
+
+
+
+
+Property of Object	Data Type of Property	value for property
+color	string	"white"
+seats	number	2
+nitros	boolean	false
+
+
+Create another variable msg which contains a string in it with the value
+"My car have seats seats and it is of color color"
+
+but you have to use the properties of the Object that you have created.
+
+Note: Generate Expected Output section will not work for this question
+You don't need to console.log anything here, just create the Car object and the msg variable.
+Input
+There is no input required for this question
+Output
+There is no output required for this question. You are only required to create the Car object and the msg string.
+ai_logoAI CODELENS
+BETA
+Simpler Explanation
+Concepts Used
+Example
+//
+//
+//
+// above is the Car object that you created
+console.log(Car) // {color: 'white', seats: 2, nitros: false}
+ */
