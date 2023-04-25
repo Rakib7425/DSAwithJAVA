7357ed62b821b826fdfe20d6f5a8b5601be574e1
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-25 14:55:14 +0530

    new file:   afterISA/javaScript/Find_the_value.js

---
+++ afterISA/javaScript/Find_the_value.js
@@ -0,0 +1,33 @@
+// Your code here
+// create array with first 5 multiples of 2
+let arr = [];
+for (let i = 1; i <= 5; i++) {
+    arr.push(2 * i);
+}
+
+// find middle element
+let n = arr.length;
+let val = arr[Math.floor((n - 1) / 2)];
+
+// console.log array and middle element
+// console.log("Array:", arr);
+// console.log("Middle element:", val);
+
+/**
+ * Find the value
+easy
+Problem Statement
+Create an array named arr which contains the first 5 multiples of 2. After this create a variable named val which stores the middle element of the array.
+
+NOTE -You don't have to return anything. You just have to initialize the variables and assign them values
+Input
+There is no input required for this question
+Output
+There is no output required for this question
+ai_logoAI CODELENS
+BETA
+Simpler Explanation
+Concepts Used
+Example
+There is no example required for this question
+ */
