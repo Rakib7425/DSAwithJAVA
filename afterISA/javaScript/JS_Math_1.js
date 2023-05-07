f8c79ea43e7df0f4493fe225baa05a60a8af16b6
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-05-07 14:14:31 +0530

    new file:   afterISA/javaScript/JS_Math_1.js 	new file:   afterISA/javaScript/JS_String_1.js 	new file:   afterISA/javaScript/hw.html

---
+++ afterISA/javaScript/JS_Math_1.js
@@ -0,0 +1,30 @@
+function round(ag) {
+    // write code here
+    // return the output , do not use console.log here
+    return Math.round(ag);
+}
+
+/** 
+ * 
+ * JS Math - 1
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Implement the function round, which should take a number which can be a float(decimal)
+and return its result as an integer rounded of (Use JS In built functions)
+Input
+Function will take a float as input (can be negative or positive)
+Output
+Function will return a rounded off number
+ai_logoAI CODELENS
+BETA
+Simpler Explanation
+Concepts Used
+Example
+console. log(round(1.112)) // prints 1
+
+console. log(round(1.9)) // prints 2
+
+console. log(round(-0.66)) // prints -1
+*/
---
+++ afterISA/javaScript/JS_String_1.js
@@ -0,0 +1,30 @@
+
+function numOfWords() {
+    // write code here
+    // return the output , do not use console.log here
+    
+}
+
+/**
+ * JS String - 1
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Implement the function numOfWords, which should take a string
+and return its result as an integer which is the number of space seperated words in it (Use JS In built functions)
+Input
+Function will take a string as argument
+Output
+Function will return the number of space separated words
+ai_logoAI CODELENS
+BETA
+Simpler Explanation
+Concepts Used
+Example
+console. log(numOfWords("Hi World")) // prints 2
+
+console. log(fnumOfWords("hi")) // prints 1
+
+console. log(numOfWords("My name is Newton")) // prints 4
+ */
